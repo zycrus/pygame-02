de3a3322760c763d28f9738a2135c8c5d7a1e5f4
Zycrus <phantom.bot.202@gmail.com>
2022-08-19 11:46:56 +0800

    Added scroll_x function

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame
+from player import Player
 from tiles import Tile
-from settings import tile_size
+from settings import tile_size, screen_width
 
 class Level:
     def __init__(self, level_data, surface):
@@ -11,14 +12,39 @@ class Level:
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
         for row_index, row in enumerate(layout):
             for col_index, cell, in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
                 if cell == "X":
-                    x = col_index * tile_size
-                    y = row_index * tile_size
                     tile = Tile((x, y), tile_size)
                     self.tiles.add(tile)
+                if cell == "P":
+                    player_sprite = Player((x, y))
+                    self.player.add(player_sprite)
+                    
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < screen_width * 0.25 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > screen_width * 0.75 and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
 
     def run(self):
+        # Tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+
+        # Player
+        self.player.update()
+        self.player.draw(self.display_surface)
+        self.scroll_x() 
