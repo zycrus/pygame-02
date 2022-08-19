1fbc7301d3edd543b2fd95ac819481adcc7326b8
Zycrus <phantom.bot.202@gmail.com>
2022-08-19 11:10:52 +0800

    zzz

---
+++ settings.py
@@ -0,0 +1,17 @@
+level_map = [
+    '                         ',
+    '                         ',
+    '                         ',
+    '                       XX',
+    ' X                     XX',
+    ' XXXX         XXXXXX   XX',
+    ' XXXX                  XX',
+    ' XXXXXXX               XX',
+    ' XXXXXXXXXXX      P    XX',
+    'XXXXXXXXXXXXX XXXXXXXXXXX',
+]
+
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
